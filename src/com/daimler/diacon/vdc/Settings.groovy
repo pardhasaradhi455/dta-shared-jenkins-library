2477d2cdc064a383281f90c7b3069c9f4992a67a
pardhasaradhi455 <pardhasaradhimalluru@gmail.com>
2025-03-10 17:49:31 +0530

    added more methods

---
+++ src/com/daimler/diacon/vdc/Settings.groovy
@@ -0,0 +1,13 @@
+def boolean checkMap(String mapName, Map checkMap, List<String> mandatoryElements, boolean mandatory){
+    mandatoryElements.each{
+        element=checkMap.get(it)
+        if (! element?.trim()){
+            if(mandatory){
+                echo "${mapName} does not contain mandatory entry: $it. Please correct and start again."
+                quiet_sh "exit 1" 
+            }else{
+                return false
+            }
+        }
+    }
+}
---
+++ vars/GoPipeline.groovy
@@ -0,0 +1,46 @@
+def call(Map pipelineParams, Map optionsMap) {
+    
+    def language = ""
+    def settings = new com.daimler.diacon.vdc.Settings()
+    switch(pipelineParams["PROG_LANGUAGE"]){
+        case "GO":
+            echo "Programming language is GO"
+            language = "GO"
+            break;
+        default:
+            echo "unknown PROG_LANGUAGE in pipelineParams: ${pipelineParams.PROG_LANGUAGE}. exiting."
+            sh "exit 1"
+    }
+
+    settings.checkMap("optionsMap",optionsMap,["COMPANY","PROJECTGROUP","REGION"],true)
+
+    if (language = "GO") {  
+        pipeline{
+            agent {
+                node {
+                    label pipelineParams["PROG_LANGUAGE"]
+                }
+            }
+            stages {
+                stage("Import libraries"){
+                    steps{
+                        sh "go mod download"
+                    }
+                }
+                stage("Test"){
+                    steps{
+                        sh "go test ."
+                    }
+                }
+                stage("Build"){
+                    steps{
+                        sh "go build ."
+                    }
+                }
+            }
+        }
+    } else { 
+        echo "Language is not supported"
+        sh "exit 1"
+    }
+}
